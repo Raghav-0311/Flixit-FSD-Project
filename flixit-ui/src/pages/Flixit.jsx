c73d60a1293fe84b9660e8c1595cceede85d68ef
Raghav Kashyap <raghavkashyap0311@gmail.com>
2023-09-06 12:31:31 +0530

    structured Navbar component

--- flixit-ui/src/pages/Flixit.jsx
+++ flixit-ui/src/pages/Flixit.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../components/Navbar';
 
 const Flixit = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+        return () => (window.onscroll = null);
+    };
+
   return (
     <div>
-      FLIXIT
+      <Navbar isScrolled={isScrolled} />
     </div>
   )
 }
