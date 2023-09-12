2f7d1259012e93b95d62e8bfee414d687109c355
Raghav Kashyap <raghavkashyap0311@gmail.com>
2023-09-12 08:28:06 +0530

    styled card component

---
+++ flixit-ui/src/components/CardSlider.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './Card'
+
+const CardSlider = ({ data, title }) => {
+  return (
+    <div className='flex'>
+      {
+        data.map((movie, index) => {
+            return <Card movieData={movie} index={index} key={movie.id} />
+        })
+      }
+    </div>
+  )
+}
+
+export default CardSlider
--- flixit-ui/src/pages/Flixit.jsx
+++ flixit-ui/src/pages/Flixit.jsx
@@ -8,6 +8,7 @@ import styled  from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMovies, getGenres } from '../store';
+import Slider from '../components/Slider';
 
 const Flixit = () => {
     const [isScrolled, setIsScrolled] = useState(false);
@@ -53,6 +54,8 @@ const Flixit = () => {
             </div>
         </div>
       </div>
+
+      <Slider movies={movies} />
     </Container>
   )
 }
