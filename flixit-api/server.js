d4f50fffef42b52e8618516a8914c608b01413af
Raghav Kashyap <raghavkashyap0311@gmail.com>
2023-09-13 07:05:14 +0530

    mongodb connection created successfully

--- flixit-api/server.js
+++ flixit-api/server.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT;
+// const DB = process.env.DB;
+const DB_CONNECTION_STRING = process.env.FLIXIT_DB_CONNECTION;
+
+mongoose
+  .connect(DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected Successfully !");
+  });
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(5000, console.log("Server Started at port 5000..."));
+app.listen(5000, console.log(`Server Started at port ${PORT}...`));
