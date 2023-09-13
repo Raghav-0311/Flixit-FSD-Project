7b1762d4ef1162f53ecd1c943bff1beaccdddd9e
Raghav Kashyap <raghavkashyap0311@gmail.com>
2023-09-13 06:32:13 +0530

    created initial express server

--- flixit-api/server.js
+++ flixit-api/server.js
@@ -1,2 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
+const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, console.log("Server Started at port 5000..."));
